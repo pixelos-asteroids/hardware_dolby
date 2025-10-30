c332782c14ca75196ae6a8910d36544872e9f1f4
hiroshi <hiroshi@tanuki.page>
2025-10-30 08:31:41 -0400

    dolby.mk: Append to ODM_MANIFEST_FILES if DEVICE_MANIFEST_FILE is empty

--- dolby.mk
+++ dolby.mk
@@ -26,9 +26,15 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DOLBY_PATH)/sepolicy/vendor
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DOLBY_PATH)/configs/vintf/dolby_framework_matrix.xml
+ifneq ($(strip $(DEVICE_MANIFEST_FILE)),)
 DEVICE_MANIFEST_FILE += \
     $(DOLBY_PATH)/configs/vintf/vendor.dolby.hardware.dms@2.0-service.xml \
     $(DOLBY_PATH)/configs/vintf/vendor.dolby.media.c2@1.0-service.xml
+else
+ODM_MANIFEST_FILES += \
+    $(DOLBY_PATH)/configs/vintf/vendor.dolby.hardware.dms@2.0-service.xml \
+    $(DOLBY_PATH)/configs/vintf/vendor.dolby.media.c2@1.0-service.xml
+endif
 
 # Build codec2 packages
 PRODUCT_PACKAGES += \
